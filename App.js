87f3db73a2f01a9a39a38f19a2629416f7f7598c
benohiit <bazelmans.benoit@gmail.com>
2024-04-13 21:26:00 +0200

    init

---
+++ App.js
@@ -0,0 +1,27 @@
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+import React, { useEffect, useState } from 'react';
+
+
+const App = () => {
+    return (
+        <SafeAreaView style={{}}>
+            <View style={styles.container}>
+                <Text>eozifhzie</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    main: {
+        backgroundColor: '#000'
+    },
+});
+
+export default App;
